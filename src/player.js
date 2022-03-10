a801c25d43ab34a97a2f49d4b4640136ad8dc7de
nacho <nacholeon30@hotmail.com>
2022-03-10 15:41:35 -0300

    improve AI

--- src/player.js
+++ src/player.js
@@ -19,12 +19,146 @@ const chooseRandomOrientation = function() {
 class Player {
   constructor() {
     this.gameboard = new Gameboard();
+    this.lastAttack = { x: null, y: null, shot: null, shipOrientation: 'unknown', foundShip: false };
   }
 
   makeRandomAttack(player) {
-    let { x, y } = chooseRandomCoordinates();
-    while(player.gameboard.receiveAttack(x, y) === 'Repeated attack.') {
-      ({ x, y } = chooseRandomCoordinates());
+    let x = this.lastAttack.x;
+    let y = this.lastAttack.y;
+    if((this.lastAttack.foundShip) && (this.lastAttack.shipOrientation === 'unknown')) {
+      do {
+        switch(Math.round(Math.random()*3)) {
+          case 0:  //up
+            if(this.lastAttack.y > 0) {
+              y = this.lastAttack.y-1;
+              break;
+            }
+          case 1:  //right
+            if(this.lastAttack.x < 9) {
+              x = this.lastAttack.x+1;
+              break;
+            }
+          case 2:  //down
+            if(this.lastAttack.y < 9) {
+              y = this.lastAttack.y+1;
+              break;
+            }
+          case 3:  //left
+            if(this.lastAttack.x > 0) {
+              x = this.lastAttack.x-1;
+              break;
+            }
+        }
+      } while(player.gameboard.receiveAttack(x, y) === 'Repeated attack.');
+
+      if(player.gameboard.record[x][y] === 'hit') {
+        if(x === this.lastAttack.x) {
+          this.lastAttack.shipOrientation = 'vertical';
+        }
+        else {
+          this.lastAttack.shipOrientation = 'horizontal';
+        }
+
+        this.lastAttack.x = x;
+        this.lastAttack.y = y;
+        this.lastAttack.shot = player.gameboard.record[x][y];
+      }
+    }
+    else if(this.lastAttack.shipOrientation === 'vertical') {
+      switch(Math.round(Math.random())) {
+        case 0:  //try up
+          y = this.lastAttack.y-1;
+          while((y > 0) && (player.gameboard.record[x][y] === 'hit')) {
+            y -= 1;
+          }
+
+          if((y >= 0) && (player.gameboard.record[x][y] !== 'miss')) {
+            break;
+          }
+
+        case 1:  //try down
+          y = this.lastAttack.y+1;
+          while((y < 9) && (player.gameboard.record[x][y] === 'hit')) {
+            y += 1;
+          }
+
+          if((y > 9) || (player.gameboard.record[x][y] === 'miss')) {
+            y = this.lastAttack.y-1;
+            while((y > 0) && (player.gameboard.record[x][y] === 'hit')) {
+              y -= 1;
+            }
+
+            if((y < 0) || (player.gameboard.record[x][y] === 'miss')) {
+              this.lastAttack.foundShip = false;
+              this.lastAttack.shipOrientation = 'unknown';
+              this.makeRandomAttack(player);
+              return;
+            }
+          }
+
+          break;
+      }
+
+      player.gameboard.receiveAttack(x, y);
+      if(player.gameboard.record[x][y] === 'hit') {
+        this.lastAttack.x = x;
+        this.lastAttack.y = y;
+        this.lastAttack.shot = player.gameboard.record[x][y];
+      }
+    }
+    else if(this.lastAttack.shipOrientation === 'horizontal') {
+      switch(Math.round(Math.random())) {
+        case 0:  //try left
+          x = this.lastAttack.x-1;
+          while((x > 0) && (player.gameboard.record[x][y] === 'hit')) {
+            x -= 1;
+          }
+
+          if((x >= 0) && (player.gameboard.record[x][y] !== 'miss')) {
+            break;
+          }
+        case 1:  //try right
+          x = this.lastAttack.x+1;
+          while((x < 9) && (player.gameboard.record[x][y] === 'hit')) {
+            x += 1;
+          }
+
+          if((x > 9) || (player.gameboard.record[x][y] === 'miss')) {
+            x = this.lastAttack.x-1;
+            while((x > 0) && (player.gameboard.record[x][y] === 'hit')) {
+              x -= 1;
+            }
+
+            if((x < 0) || (player.gameboard.record[x][y] === 'miss')) {
+              this.lastAttack.foundShip = false;
+              this.lastAttack.shipOrientation = 'unknown';
+              this.makeRandomAttack(player);
+              return;
+            }
+          }
+
+          break;
+      }
+
+      player.gameboard.receiveAttack(x, y);
+      if(player.gameboard.record[x][y] === 'hit') {
+        this.lastAttack.x = x;
+        this.lastAttack.y = y;
+        this.lastAttack.shot = player.gameboard.record[x][y];
+      }
+    }
+    else {
+      do {
+        ({ x, y } = chooseRandomCoordinates());
+      } while(player.gameboard.receiveAttack(x, y) === 'Repeated attack.');
+
+      if(player.gameboard.record[x][y] === 'hit') {
+        this.lastAttack.foundShip = true;
+        this.lastAttack.x = x;
+        this.lastAttack.y = y;
+      }
+
+      this.lastAttack.shot = player.gameboard.record[x][y];
     }
   }
 
