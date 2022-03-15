9afce00d7e359e2e36ca68d88308ea64464de0cb
nacho <nacholeon30@hotmail.com>
2022-03-15 11:32:47 -0300

    improve visuals

--- src/index.js
+++ src/index.js
@@ -11,27 +11,27 @@ homeScreen.playButton.addEventListener('click', playerScreen.load);
 playerScreen.onePlayerButton.addEventListener('click', function() {
   shipPlacementScreen.load();
   shipPlacementScreen.addReadyHandler(function() {
-    const ships = shipPlacementScreen.getShips();  //[{x, y, orientation, size}, {...}, {...}, ...]
-    game.placeShips(ships);
-    gameScreen.load();
-    game.start();
-    gameScreen.updateLayout(game.player1.gameboard.layout);
-    gameScreen.addRecordHandler(function(x, y) {
-      if(game.doPlayerTurn(x, y)) {
-        gameScreen.updateRecord(game.computer1.gameboard.record);
-        if(game.checkStatus() === 'player wins') {
-          gameScreen.loadGameOverScreen('player');
-        }
-        else {
-          game.doPcTurn();
-          gameScreen.updateLayout(game.player1.gameboard.record);
-          if(game.checkStatus() === 'pc wins') {
-            gameScreen.loadGameOverScreen('pc');
+    if(shipPlacementScreen.areAllShipsPlaced()) {
+      const ships = shipPlacementScreen.getShips();
+      game.placeShips(ships);
+      gameScreen.load();
+      game.start();
+      gameScreen.updateLayout(game.player1.gameboard.layout);
+      gameScreen.addRecordHandler(function(x, y) {
+        if(game.doPlayerTurn(x, y)) {
+          gameScreen.updateRecord(game.computer1.gameboard.record);
+          if(game.checkStatus() === 'player wins') {
+            gameScreen.loadGameOverScreen('player');
+          }
+          else {
+            game.doPcTurn();
+            gameScreen.updateLayout(game.player1.gameboard.record);
+            if(game.checkStatus() === 'pc wins') {
+              gameScreen.loadGameOverScreen('pc');
+            }
           }
         }
-      }
-    });
+      });
+    }
   });
 });
-
-//THINK IT BACKWARDS
